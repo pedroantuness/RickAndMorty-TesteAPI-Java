f95009057058e7cd9940547e203dd1102491e867
pedroantuness <RM551409@fiap.com.br>
2023-09-28 10:03:56 -0300

    first commit

---
+++ src/main/java/com/example/Personagem.java
@@ -0,0 +1,118 @@
+package com.example;
+
+public class Personagem {
+    
+    private int id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private Object origin;
+    private Object location;
+    private String image;
+    private Object episode;
+    private Object url;
+    private String created;
+    
+    public Personagem(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String nome) {
+        this.name = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Object getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Object origin) {
+        this.origin = origin;
+    }
+
+    public Object getLocation() {
+        return location;
+    }
+
+    public void setLocation(Object location) {
+        this.location = location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Object getEpisode() {
+        return episode;
+    }
+
+    public void setEpisode(Object episode) {
+        this.episode = episode;
+    }
+
+    public Object getUrl() {
+        return url;
+    }
+
+    public void setUrl(Object url) {
+        this.url = url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+}
